8a7cb9196d4940e911799239b405f698fba9ce95
cmdeao <47462906+cmdeao@users.noreply.github.com>
2020-08-07 18:07:41 -0400

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Activity1Part3.Models;
+using Activity1Part3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1Part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        //[HttpGet]
+        //public string Index()
+        //{
+        //    //return View();
+        //    return @"<b>Just a test from Index</b>";
+        //}
+
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Login");
+            }
+
+            SecurityService service = new SecurityService();
+            bool results = service.Authenticate(model);
+
+            if(results)
+            {
+                return View("LoginPassed");
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+            //return "Here " + model.Username + " and " + model.Password;
+        }
+    }
+}
