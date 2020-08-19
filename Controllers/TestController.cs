5bf28a968d542328bfd3e88bb52a73d7210261fa
cmdeao <47462906+cmdeao@users.noreply.github.com>
2020-08-19 12:36:36 -0400

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> users = new List<UserModel>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                UserModel user;
+                user = new UserModel("test " + i.ToString(), "email " + i.ToString(), "12345678" + i.ToString());
+                users.Add(user);
+            }
+
+            return View("Test", users);
+        }
+    }
+}
