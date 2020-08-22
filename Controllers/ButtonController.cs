52591b6f1a4bfe5461a3dd68327d7232c716bad9
cmdeao <47462906+cmdeao@users.noreply.github.com>
2020-08-22 17:26:28 -0400

    Add files via upload

---
+++ Controllers/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class ButtonController : Controller
+    {
+        public static List<ButtonModel> buttons = new List<ButtonModel>();
+        // GET: Button
+        public ActionResult Index()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                buttons.Add(new ButtonModel(true));
+            }
+
+            return View("Button", buttons);
+        }
+
+        public ActionResult OnButtonClick(string mine)
+        {
+            int value = Int32.Parse(mine);
+
+            if(value == 1)
+            {
+                buttons[0].State = !buttons[0].State;
+            }
+            else
+            {
+                buttons[1].State = !buttons[1].State;
+            }
+
+            return View("Button", buttons);
+        }
+    }
+}
