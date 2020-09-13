8b3d32d7a7bac560883544772a7ade19df013ae4
cmdeao <47462906+cmdeao@users.noreply.github.com>
2020-09-12 21:30:20 -0500

    Add files via upload

---
+++ Services/Utility/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Services.Utility
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+}
